28ec14d5d03d7c8332f1df5e0d1d443584ac212d
Jasmine <jazicorndev@gmail.com>
2022-02-07 11:34:42 -0500

    completed drawing class

--- product-app/src/main/java/Hangman/Drawings.java
+++ product-app/src/main/java/Hangman/Drawings.java
@@ -6,50 +6,42 @@ public class Drawings {
         switch(step) {
             case (0) -> getBase();
             case (1) -> getStep1();
+            case (2) -> getStep2();
+            case (3) -> getStep3();
+            case (4) -> getStep4();
+            case (5) -> getStep5();
+            case (6) -> getStep6();
         }
     }
 
-    String getBase() {
-        String base;
-        base = String.format("%s\n",
-                """
-                    +---+
+    private String base = String.format("%s\n",
+            """
+                +---+
+                
+                    |
                     
-                        |
-                        
-                        |
-                        
-                        |
-                        
-                        = = =
-                """);
-
-        return base;
-    }
-
-    String getStep1() {
-        String step1;
-
-        step1 = String.format("%s\n",
-                """
-                    +---+
+                    |
                     
-                    O   |
-                        
-                        |
-                        
-                        |
-                        
-                        = = =
-                """);
+                    |
+                    
+                    = = =
+            """);
 
-    }
+    private String step1 = String.format("%s\n",
+            """
+                +---+
+                
+                O   |
+                    
+                    |
+                    
+                    |
+                    
+                    = = =
+            """);
 
-    static void step2() {
-        String step2;
-
-        step2 = String.format("%s\n",
-                """
+    private String step2 = String.format("%s\n",
+            """
                     +---+
                     
                     O   |
@@ -61,33 +53,102 @@ public class Drawings {
                         = = =
                 """);
 
+
+    private String step3 = String.format("%s\n",
+            """
+                +---+
+                
+                O   |
+                  /
+                |   |
+                    
+                    |
+                    
+                    = = =
+            """);
+
+    private String step4 = String.format("%s\n",
+            """
+                +---+
+                
+                O   |
+              \\  /
+                |   |
+                    
+                    |
+                    
+                    = = =
+            """);
+
+    private String step5 = String.format("%s\n",
+            """
+                +---+
+                
+                O   |
+              \\  /
+                |   |
+                  \\
+                    |
+                    
+                    = = =
+            """);
+
+    private String step6 = String.format("%s\n",
+            """
+                +---+
+                
+                O   |
+              \\  /
+                |   |
+              //  \\
+                    |
+                    
+                    = = =
+            """);
+
+    public String getBase() {
+        return base;
     }
-    static void step3() {
-        String step3;
-
-        step3 = String.format("%s\n",
-                """
-                step 3
-                """);
-
+    public String getStep1() {
+        return step1;
     }
-    static void step4() {
-        String step4;
-
-        step4 = String.format("%s\n",
-                """
-                step 4
-                """);
-
+    public String getStep2() {
+        return step2;
     }
-    static void step5() {
-        String step5;
-
-        step5 = String.format("%s\n",
-                """
-                step 5
-                """);
-
+    public String getStep3() {
+        return step3;
     }
+    public String getStep4() {
+        return step4;
+    }
+    public String getStep5() {
+        return step5;
+    }
+    public String getStep6() {
+        return step6;
+    }
+
+    public void setBase (String base) {
+        this.base = base;
+    }
+    public void setStep1 (String step1) {
+        this.step1 = step1;
+    }
+    public void setStep2 (String step2) {
+        this.step2 = step2;
+    }
+    public void setStep3 (String step3) {
+        this.step3 = step3;
+    }
+    public void setStep4 (String step4) {
+        this.step4 = step4;
+    }
+    public void setStep5 (String step5) {
+        this.step5 = step5;
+    }
+    public void setStep6 (String step6) {
+        this.step6 = step6;
+    }
+
 
 }
