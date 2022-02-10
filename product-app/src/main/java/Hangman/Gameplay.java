21318ef34da568ee77b23e3ee5211e6b951a6698
Jasmine <jazicorndev@gmail.com>
2022-02-10 17:25:31 -0500

    created file and pseudocode

---
+++ product-app/src/main/java/Hangman/Gameplay.java
@@ -0,0 +1,32 @@
+package Hangman;
+
+import java.io.IOException;
+
+public class Gameplay {
+
+    public Gameplay() throws IOException {
+
+    }
+
+    public void test() {
+
+        System.out.println();
+        System.out.println("Test Statements: Gameplay");
+        System.out.println();
+        System.out.println();
+        System.out.println();
+    }
+
+    Gameword gameword = new Gameword();
+
+    // game rules
+    // give player word
+    // player guess letter
+    // you let player know if guess is correct
+    // if guess wrong add body part to picture
+    // take away player guess
+    // if player guess word they win
+    // if player guess wrong man is dead
+    // ask player if they want to play again
+
+}
